4efafae84aec88f27bc80ee064b4f006644b292b
Adby Santos <32530299+BlackLeg15@users.noreply.github.com>
2017-10-05 22:25:39 -0300

    Update usuario.c

--- usuario.c
+++ usuario.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include "usuario.h"
 
+struct arv{
+    ArvNo* raiz;
+};
+
+struct arvno{
+    Usuario* usuario;
+    ArvNo* esq;
+    ArvNo* dir;
+};
+
 struct usuario{
     char nome[51];
     int perfil;
@@ -26,7 +36,44 @@ Usuario* usu_cria(char* nome, int perfil, int id, char* login, char* senha)
     return u;
 }
 
-void realiza_cadastro()
+void realiza_cadastro(Sistema* sys)
 {
     printf("Digite seu nome: ");
+    char* n = le_string();
+    printf("Digite seu login: ");
+    char* l = le_string();
+    printf("Digite sua senha: ");
+    char* s = le_string();
+    printf("Digite seu CPF: ");
+    char* i = le_string();
+    printf("Qual seu perfil?\nDigite (1) para aluno e (2) para professor: ");
+    char* p = le_string();
+    Usuario* u = usu_cria(n, p, i, l, s);
+    return U;
+}
+
+static ArvNo* insere(ArvNo* a, Usuario* u)
+{
+    if(a == NULL){
+        ArvNo* a = (ArvNo* a) malloc(sizeof(ArvNo*);
+        if(a == NULL){
+            printf("erro - 'insere'\n");
+            exit(1);
+        }
+        a->info = u;
+        a->esq = NULL;
+        a->dir = NULL;
+        return a;
+    }
+    if(strcmp(u->nome, a->info->nome) < 0)
+        return a->esq = insere(a->esq, u);
+    if(strcmp(u->nome, a->info->nome) > 0)
+        return a->dir = insere(a->dir, u);
+
+    return a;
+}
+
+void usuario_insere (Sistema* sys, Usuario* u)
+{
+    sys->raiz = insere(sys->raiz, u);
 }
